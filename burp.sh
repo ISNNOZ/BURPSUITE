2fb05dc320d2dbbdfa882c019fcbf68b6f83c7c1
Manav <88114624+ISNNOZ@users.noreply.github.com>
2023-10-28 14:13:46 +0530

    Add files via upload

---
+++ burp.sh
@@ -0,0 +1,2 @@
+java --illegal-access=permit -Dfile.encoding=utf-8 -javaagent:BurpSuiteLoader_v2020.12.jar -noverify -jar burpsuite_pro_v2020.12.jar
+Picked up _JAVA_OPTIONS: -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true
